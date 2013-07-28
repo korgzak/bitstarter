072a9cc75adf5d9b721b138a9a1726d472ce2c81
Ubuntu <ubuntu@ip-172-31-33-64.us-west-2.compute.internal>
2013-07-28 14:48:04 +0000

    still not working

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -26,6 +27,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest =  require('restler');
+var sys =require ('util');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +68,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to web html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    rest.get(program.url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    sys.puts('Error: ' + result.message);
+	    // this.retry(5000); // try again after 5 sec
+	} else {
+	    sys.puts(result);
+	    $ = cheerioHtmlFile(result);                                                                                                                                                                   var checks = loadChecks(checksfile).sort();                                                                                                                                                    var out = {};                                                                                                                                                                                  for(var ii in checks) {                                                                                                                                                                     
+		var present = $(checks[ii]).length > 0;                                                                                                                                                        out[checks[ii]] = present;                                                                                                                                                              
+	    }
+
+	}
+    });
+
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+//    var outJson = JSON.stringify(out, null, 4);
+  //  console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
